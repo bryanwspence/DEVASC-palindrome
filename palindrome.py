90bb60872054eee2d040fbf68b9e7f878929aa20
bryanwspence <bryan.w.spence@gmail.com>
2020-06-03 12:42:06 -0400

    tweaked palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,17 @@
-# Create emtpy list
-palindrome_list = []
-
 # Ask for input
+user_palindrome = input("Please enter a word: ")
 
-user_palindrome = str(input("Please enter a word ")
+# Reverse palindrom_list
+reverse_palindrome = user_palindrome.lower()[::-1]
+
+# Print word
+print("\nYour word is {}".format(user_palindrome))
+print("\nYour word backwards is {}".format(reverse_palindrome))
+
+if user_palindrome.lower() == reverse_palindrome:
+    print("\n{} is a palindrome!".format(user_palindrome))
+
+else:
+    print("\n{} is not a palindrome".format(user_palindrome))
+
+
